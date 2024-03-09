5768d3b1d0e9544c62b0a1d4abaebb608d0a3b6d
Ross McFarland <rwmcfa1@gmail.com>
2024-03-09 14:53:29 -0800

    Import skeleton

---
+++ tests/test_provider_octodns_keyring.py
@@ -0,0 +1,14 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_keyring import KeyringSecrets
+
+
+class TestKeyringSecrets(TestCase):
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        KeyringSecrets
