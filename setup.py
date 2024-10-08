0ea57310b4b96b7c3f4dbe23cf365dfa7367acaf
Ross McFarland <rwmcfa1@gmail.com>
2024-10-08 11:36:21 -0700

    Bump python_requires to 3.9

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     long_description_content_type='text/markdown',
     name='octodns-keyring',
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     tests_require=tests_require,
     url='https://github.com/octodns/octodns-keyring',
     version=version(),
